8401e5030529983d8e641bcdda392bdf6be338c7
YuviPanda <yuvipanda@gmail.com>
2022-12-04 22:45:10 -0800

    ADD EVIL MODE

--- emacs/init.el
+++ emacs/init.el
@@ -13,6 +13,10 @@
 
 ;; Without this, you need Ctrl-G or something to kill any particular command.
 ;; Fuxk that, that's what my ESC key is for
+;; the `kbd` function converts a human-readable keycode thing to whatever emacs needs
+;; global-set-key is for *everything*, while define-key is for specific keymaps used by specific modes
+;; FIXME: Find out more about keymaps and modes and how they relate
+;; FIXME: But generally use general.el instead?
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 ;; Set up our font.
@@ -24,7 +28,7 @@
 
 ;; Setu package management
 (require 'package) ; This is the built in, meh package manager within emacs
-                                        ; TODO: Figure out what this gets stuff from.
+;; TODO: Figure out what this gets stuff from.
 (package-initialize) ; Initialize the built in package manager
 (unless package-archive-contents
   (package-refresh-contents)) ; Equivalent of apt-get update
@@ -73,6 +77,10 @@
   (doom-themes-org-config)
   (load-theme 'doom-henna t)  ; The second param being t marks it as safe
   )
+
+;; Provide icon packs for doom-themes and other packages
+;; FIXME: Find out how to automatically install these if needed?
+(use-package all-the-icons)
 
 ;; Enable line numbers!
 (global-display-line-numbers-mode t)
@@ -126,6 +134,57 @@
 ;; From https://emacsredux.com/blog/2020/12/04/maximize-the-emacs-frame-on-startup/
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
+
+;; EVIL MODE HERE WE FUCKING COME BITCHES!
+
+(use-package evil
+  :init
+  (setq evil-want-integration t) ; Integrate with something? FIXME: Find out what we are integrating with?
+  (setq evil-want-keybinding nil) ; Don't add some random keybindings? idk what that is either.
+  :config
+  (evil-mode 1)
+
+  ;; Make j and k work on what looks like lines on screen, not what looks like lines on text
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  )
+;;
+;; Setup general for keymaps
+(use-package general
+  :config
+  ;; Tell it we wanna use it with evil
+  (general-evil-setup t)
+
+  ;; Define a custom function that can be used to define our custom functions
+  (general-create-definer yuvi/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC"
+    )
+
+  ;; THIS IS WHERE OUR KEYBINDINGS ARE OMG OMG OMG
+  ;; Let's keep this as minimal as possible, and rely on M-x for things we won't do often (like switching themes)
+  (yuvi/leader-keys
+   ;; TOP LEVEL KEYBINDINGS FUCK YES!!!!!! OMG
+   ";" '(counsel-M-x :which-key "M-x")
+   "SPC" '(counsel-switch-buffer :which-key "Switch buffers")
+
+   ;; Window specific keybindings. In the non-emacs world, i'd call these 'frames' lol
+   "w" '(:ignore t :which-key "window")
+   "wv" '(split-window-vertically :which-key "vertical-split")
+   "wh" '(split-window-horizontally :which-key "horizontal-split")
+   "wd" '(delete-window :which-key "close")
+   )
+  )
+
+;; Sets up evil keybindings for a lot of modes, like helpful
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init)
+  )
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -134,7 +193,7 @@
  '(custom-safe-themes
    '("683b3fe1689da78a4e64d3ddfce90f2c19eb2d8ab1bab1738a63d8263119c3f4" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" default))
  '(package-selected-packages
-   '(counsel which-key use-package rainbow-delimiters ivy-rich doom-modeline)))
+   '(evil-collection counsel which-key use-package rainbow-delimiters ivy-rich doom-modeline)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
